1ce3afb171f74cb78b9ff314ff8900cb16995e03
mnurhuda08 <mnurhuda009@gmail.com>
2024-01-02 12:59:49 +0700

    Add project files.

---
+++ WEBAPP/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPP.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id.HasValue) { 
+            return new ContentResult { Content = id.ToString() };
+            }
+            else
+            {
+            return new ContentResult { Content = "ID Not Found" };
+
+            }
+        }
+    }
+}
